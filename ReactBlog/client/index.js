cc65309b01f9568e05e6030c1e1228876cad08d7
Jawahar S <jawahar@GIHOS.COM>
2016-09-01 18:39:13 +0530

    add react-thunk

--- ReactBlog/client/index.js
+++ ReactBlog/client/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { Router, Route, browserHistory } from 'react-router';
-import routes from './routes'
+import routes from './routes';
 
-render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
+const store = createStore((state = {}) => state, applyMiddleware(thunk));
+
+render(
+	<Provider store={store}>
+		<Router history={browserHistory} routes={routes} />
+	</Provider>, 
+	document.getElementById('app')
+);
